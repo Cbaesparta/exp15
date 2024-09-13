68447db0a2f78c56224848fc2e736096fba88ef6
Sai Sankar Jena <saisankarjena4568@gmail.com>
2024-09-13 10:04:57 +0530

    Add files via upload

---
+++ rev3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+void reverse(char *str){
+    if(*str){
+        reverse(str+1);
+        cout<<("%c",*str);
+    }
+}
+int main(){
+    char a[50];
+    cout<<"enter a string";
+    cin>>a;
+    reverse(a);
+    return 0;
+}
